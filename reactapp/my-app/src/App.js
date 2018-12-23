dc8b5e9a524f26f063c13d03c2ed73b133ce7421
amolnadagonde <amol.nadagonde@gmail.com>
2018-12-23 21:08:24 +0530

    trying react apps
    testing creat-react-app with github pages

---
+++ reactapp/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Welcome to create-react-app</div>
+    );
+  }
+}
+
+export default App;
